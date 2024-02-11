f2efd76dd536c5c9b2ce34d76c106875df9311c1
darofall <darofaalll@gmail.com>
2024-02-11 22:01:01 +0100

    renommer avance

--- src/main/java/SimpleClasse.java
+++ src/main/java/SimpleClasse.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SimpleClasse {
+public class SimpleClasse {
+    public static void main(String[] args) {
+        new SimpleClasse().add(1, 2);
+    }
+    /**
+     * Adds a and b
+     *
+     * @param firstNumber the first number
+     * @param b           the second number
+     */
+    public int add(int firstNumber, int b) {
+        return firstNumber + b;
+    }
+
 }
