5ce802f11a45207cdef2aca17bb98156d2ac3b3c
darofall <darofaalll@gmail.com>
2024-02-11 22:18:43 +0100

    extraction des variables

--- src/main/java/SimpleClasse.java
+++ src/main/java/SimpleClasse.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class SimpleClasse {
     public static void main(String[] args) {
         new SimpleClasse().add(1, 2);
@@ -11,5 +13,10 @@ public class SimpleClasse {
     public int add(int firstNumber, int b) {
         return firstNumber + b;
     }
-
+    public static boolean isNowBetween(LocalDate startingDate, LocalDate endingDate)
+    {
+        LocalDate now = LocalDate.now();
+        return now.isAfter(startingDate) &&
+                now.isBefore(endingDate);
+    }
 }
